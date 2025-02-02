582793e276222b5e8758e7cca3c61dfa37a61459
rich11berr <rinocraft.tv@gmail.com>
2025-02-02 13:17:22 +0400

    signals

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,26 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, effect, signal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
+
+  public counter = signal<number>(0);
+
+  public doubleCounter = computed(() => {
+    return this.counter() * 2
+  })
+
+  constructor() {
+    effect(() => {
+      console.log(`signal tgriggered, ${this.counter()}`)
+    })
+  }
+
+  public plusCounter(): void {
+    this.counter.update(value => value + 1);
+  }
 }
