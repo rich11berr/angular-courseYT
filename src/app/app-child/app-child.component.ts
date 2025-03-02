78e7178814de633c69bd52a07ab1c932655c5f15
rich11berr <rinocraft.tv@gmail.com>
2025-03-02 20:14:56 +0400

    linkedSignals

--- src/app/app-child/app-child.component.ts
+++ src/app/app-child/app-child.component.ts
@@ -1,4 +1,4 @@
-import { Component, Output, computed, input, model, output } from '@angular/core';
+import { Component, Output, Signal, computed, input, linkedSignal, model, output, signal } from '@angular/core';
 
 @Component({
   selector: 'app-child',
@@ -14,5 +14,27 @@ export class AppChildComponent {
     this.value.update(oldValue => oldValue + 1)
   }
 
+
+
+  public shippingAddresses = signal<string[]>(['New-York', 'London', 'Amsterdam', 'Paris']);
+
+  // public selectedOption = signal<string>(this.shippingAddresses()[0])
+  // public selectedOption = linkedSignal(() => this.shippingAddresses()[0])
+  public selectedOption = linkedSignal<string[], string>({
+    source: this.shippingAddresses,
+    computation: (newOptions, previous) => {
+      console.log(previous);
+      return newOptions[0]
+    },
+    equal: (a, b) => a.length < b.length
+  })
+
+  public changeShippingAddress(index: number): void {
+    this.selectedOption.set(this.shippingAddresses()[index])
+  }
+
+  public changeAddresses(): void {
+    this.shippingAddresses.set(['Moscow', 'Novgorod', 'Samara'])
+  }
 }
 
